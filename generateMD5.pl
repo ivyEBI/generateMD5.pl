ec107d6ab1f12eeb62e1b9de9718c20efc333f43
Ivy Ding <ivyding@ebi.ac.uk>
2018-09-05 15:26:51 +0100

    Add files via upload

--- generateMD5.pl
+++ generateMD5.pl
@@ -13,7 +13,8 @@ while (<$commandFile>){
         $parameterArray[$count][0] = $para[0];
         $parameterArray[$count][1] = $para[1];
 }
-my @resultPEA =`mysql -u$parameterArray[0][0] -p$parameterArray[0][1] -hpg-mysql-vault-ega-drupal.ebi.ac.uk -P4357 prod_ega_accounts -e "select file_name,stable_id from prod_ega_accounts.file where length(unencrypted_md5) <> 32"`;
+print "EGAF_ID\tthe_wrong_md5\tthe_correct_md5/mismatch\n";
+my @resultPEA =`mysql -u$parameterArray[0][0] -p$parameterArray[0][1] -hpg-mysql-vault-ega-drupal.ebi.ac.uk -P4357 prod_ega_accounts -e "select file_name,stable_id,unencrypted_md5 from prod_ega_accounts.file where length(unencrypted_md5) <> 32"`;
 my $row_count =0;
 foreach my $row (@resultPEA){
         $row_count++;
@@ -27,10 +28,11 @@ foreach my $row (@resultPEA){
         close THEFILE;
 	my @ele1 = split(/\s+/,$firstLine);
 	my @md5s = `mysql -u$parameterArray[1][0] -p$parameterArray[1][1] -hpg-mysql-vault-ega-prod -P4189 ega_audit_ve_vault_archive_test -e "select md5_checksum from audit_md5 where file_stable_id = '$ele[1]' and process_step = 'Archived_file unencrypted MD5'"`;
-        chomp($md5s[1]);
+        if (defined $md5s[1]){chomp($md5s[1]);} else{$md5s[1] = "";}
+        print $ele[1],"\t",$ele[2],"\t",$md5s[1],"\t";
 	if ($ele1[0] eq $md5s[1]){
-            my $command1 = `mysql -u$parameterArray[2][0] -p$parameterArray[2][1] -hpg-mysql-vault-ega-drupal.ebi.ac.uk  -P4357 prod_ega_accounts_test -e "update file set unencrypted_md5='$ele1[0]' where stable_id ='$ele[1]'"`;
-            
-        }else{print $ele[1],"---- mismatch\n"}
+            my $command1 = `mysql -u$parameterArray[2][0] -p$parameterArray[2][1] -hpg-mysql-vault-ega-drupal.ebi.ac.uk  -P4357 prod_ega_accounts -e "update file set unencrypted_md5='$ele1[0]' where stable_id ='$ele[1]'"`;
+            print "\n";
+        }else{print "mismatch\n"}
 }
 
